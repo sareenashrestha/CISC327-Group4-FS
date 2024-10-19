c7b4bb10237a30f17dbfd7a7559c016c49b152a0
Sabin Pokhrel <118865601+sabinpok@users.noreply.github.com>
2024-10-19 14:25:28 -0400

    added login html file and beginning of functionality

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
 
@@ -8,6 +8,26 @@ def index():
         user_input = request.form['user_input']
         return render_template('index.html', user_input=user_input)
     return render_template('index.html', user_input=None)
+
+
+dummy = {
+    'user1': 'password1',
+    'user2': 'password2',
+    'user3': 'password3'
+}
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in dummy and dummy[username] == password:
+            flash("Login successful, welcome {username}")
+            return redirect(url_for('index'))
+        else:
+            flash("Username or password is incorrect")
+            return redirect(url_for('login'))
+            
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
