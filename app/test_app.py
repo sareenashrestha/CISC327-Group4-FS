a06676d69541015f38fad29b9980c6d4fb94797d
sareenashrestha <sareenashrestha@hotmail.com>
2024-10-22 22:56:22 -0400

    minor changes to login and registration test cases

--- app/test_app.py
+++ app/test_app.py
@@ -15,7 +15,7 @@ class TestLogin(unittest.TestCase):
     # Testing for when a user successfully logs in, with their email and password in the dummy hashmap
     def test_successful_login(self):
         # Redirecting is false, as the check for the flash message should be done before the redirest
-        response = self.app.post('/login', data=dict(username='user1@gmail.com', password='password1'), follow_redirects=False)
+        response = self.app.post('/login', data=dict(username='user1@gmail.com', password='Password1!'), follow_redirects=False)
         self.assertEqual(response.status_code, 302) # 302 is the status code for redirect
 
         # This will grab the session data, which will include the flash message, then check if the right flash message is in the session data
@@ -61,7 +61,7 @@ class TestRegistration(unittest.TestCase):
             termsCheck='on'
         ), follow_redirects=True)
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Login', response.data)
+        self.assertIn(b'Email Address', response.data)
 
     # test for when a user enters an invalid email
     def test_invalid_email(self):
