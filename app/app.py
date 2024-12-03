d5738bd49fe12c85c5de83ec3e1dd86485b83c04
Sareena Shrestha <sareenashrestha@Sareenas-MacBook-Pro.local>
2024-12-03 13:16:40 -0500

    added host and port configuration to flask app

--- app/app.py
+++ app/app.py
@@ -10,8 +10,6 @@ app.config['SECRET_KEY'] = 'your_secret_key_here'
 
 init_db()
 
-init_db()
-
 # Validation regex patterns
 email_regex = r'^[^\s@]+@[^\s@]+\.[^\s@]+$'
 password_regex = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$'
@@ -183,6 +181,5 @@ def cancel_booking(booking_id):
     flash('Booking canceled successfully!', 'success')
     return redirect(url_for('cancelBooking'))
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=5000)
